b38a665f5aa25eebcae5dc300b2eba4bf9935122
Charlie White <charlie@anchor.io>
2013-09-07 15:18:09 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BluenoteRails::Application.config.secret_key_base = 'ba4089e9ddf0c32a81d12d3c8a0e1358100650c6d6849e4bb001a80a3d100a8be12c54b8106d03af73192ed413e9b2939debc3e4e48ffe7cf4c693e9d81dc8cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BluenoteRails::Application.config.session_store :cookie_store, key: '_bluenote-rails_session'
