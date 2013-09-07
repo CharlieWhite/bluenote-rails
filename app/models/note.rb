4eb6e8a2c1de9ead8196c6bd6e618d64a68cc93e
Charlie White <charlie@anchor.io>
2013-09-07 16:59:08 -0700

    fixing public notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,6 +3,6 @@ class Note < ActiveRecord::Base
 	belongs_to :sent_user, :class_name => "User", :foreign_key => "from_user_id"
 	belongs_to :received_user, :class_name => "User", :foreign_key => "user_id"
 
-	scope :public, ->  { where(:user_id => nil) }
+	scope :public, ->  { where(:user_id => "") }
 
 end
